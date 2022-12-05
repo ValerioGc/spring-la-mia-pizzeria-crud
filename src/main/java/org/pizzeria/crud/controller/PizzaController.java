1443df06348f2af64544680471e2af46a886be1e
ValerioGc <valerio.gc1@gmail.com>
2022-12-05 22:43:21 +0100

    Avanzamento crud

--- src/main/java/org/pizzeria/crud/controller/PizzaController.java
+++ src/main/java/org/pizzeria/crud/controller/PizzaController.java
@@ -1,6 +1,7 @@
 package org.pizzeria.crud.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.pizzeria.crud.pojo.Pizza;
 import org.pizzeria.crud.serv.PizzaService;
@@ -9,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -24,6 +30,67 @@ public class PizzaController {
 		
 		List<Pizza> pizzas = pizzaService.findAll();
 		model.addAttribute("pizzas", pizzas);
+		model.addAttribute("routeName", "index");
 		return "index";
+	}
+	
+	@GetMapping("/home")
+	public String goHome(Model model) {
+		model.addAttribute("routeName", "home");
+		return "home" ;
+	}
+	
+	
+	@GetMapping("/pizza/{id}")
+	public String editPizza(@PathVariable("id") int id) {
+		
+		return "pizza" ;
+	}
+	
+	
+	
+	@GetMapping("/pizza/newPizza")
+	public String createPizza(Model model) {
+		
+		Pizza pizza = new Pizza();
+		model.addAttribute("pizza", pizza);
+		model.addAttribute("routeName", "new");
+		
+		return "newPizza";
+	}
+	
+	@PostMapping("/pizza/newPizza")
+	public String storePizza(@Valid @ModelAttribute("pizza") Pizza pizza) {
+		
+		pizzaService.save(pizza);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/pizza/edit/{id}")
+	public String editPizza(@PathVariable("id") int id, Model model) {
+		
+		Optional<Pizza> optPizza = pizzaService.findPizzaById(id);
+		Pizza pizza = optPizza.get();
+		
+		model.addAttribute("pizza", pizza);
+		model.addAttribute("routeName", "edit");
+		
+		return "editPizza";
+	}
+	
+	@PostMapping("/pizza/edit")
+	public String updatePizza(@Valid @ModelAttribute("pizza") Pizza pizza) {
+		
+		pizzaService.save(pizza);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/pizza/delete/{id}")
+	public String deletePizza(@PathVariable("id") int id) {
+		
+		pizzaService.deletePizzaById(id);
+		
+		return "redirect:/";
 	}
 }
